b8583c78a3a77e24e6a960c879553c6bf98d9919
Guillermo Herrera-Arcos <guillermo_h95@hotmail.com>
2017-12-27 13:31:46 -0600

    Arduino codes

---
+++ FlexSensorTransmitter.ino
@@ -0,0 +1,87 @@
+//   [2]   [12]
+//   [11]  [13]
+//   [10]  [ 9]
+//  [3.3V]|[GND]|
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#define CE_PIN   9
+#define CSN_PIN 10
+const uint64_t pipe = 0xE8E8F0F0E1LL; 
+
+RF24 radio(CE_PIN, CSN_PIN);
+int datas[2]; 
+int val,valMod;
+int data[20];
+int temp[20];
+
+void setup() 
+{
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(pipe);
+}
+
+void loop()  
+{
+  val=analogRead(A0);
+  for(int i=0;i<20;i++){
+    data[i]=analogRead(A0);
+    //Serial.println(data[i]);
+  }
+  merge(0,19);
+  //Serial.println(data[10]);
+  valMod=map(data[10],0,1023,0,255);
+  Serial.println(valMod);
+  
+  for(int i=0;i<20;i++){
+    data[i]=0;
+    //Serial.println(data[i]);
+  }
+  //Serial.println(val);
+  //data[1] = 4;
+  datas[0]=valMod;
+  //Serial.println(valMod);
+  radio.write(datas, sizeof(datas));
+}
+
+//----------------------------------------------------------------------------------
+void merge(int b,int e){
+   int m;
+   if(b<e){
+     m=(b+e)/2;
+     merge(b,m);
+     merge(m+1,e);
+     join(b,m,e);
+   }
+}
+//----------------------------------------------------------------------------------
+void join(int ini, int medio, int fin){
+  int i=ini;
+  int j=medio+1;
+  int k=fin;
+  while(i<=medio && j<=fin){
+    if(data[i]<data[j]){
+       temp[k] = data[i];
+       i++;}
+    else{
+      temp[k]=data[j];
+      j++;}
+    k++;
+  }
+    
+  if(i>medio){
+    for(int l=j;l<=fin;l++){
+      temp[k]=data[l];
+      k++;}}
+  else{
+    for(int u=i;u<=medio;u++){
+      temp[k]=data[u];
+      k++;}}
+  for(int p=0;p<20;p++){
+    data[p]=temp[p];}
+}
+  
+  
+
---
+++ RoboticHand_Code.ino
@@ -0,0 +1,59 @@
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5; //Define servos
+
+#define dedo1 A1
+#define dedo2 A2
+#define dedo3 A3
+#define dedo4 A4
+#define dedo5 A5 //Define analog inputs for flex sensors 
+
+int val1;
+int val2;
+int val3;
+int val4;
+int val5; //Define variables for reading finger values
+
+void setup() {
+servo1.attach(1);
+servo2.attach(2);
+servo3.attach(3);
+servo4.attach(4);
+servo5.attach(5); //Define servo's outputs
+
+}
+
+void loop() {
+    
+     
+val1 = analogRead(dedo1);
+val1 = map(val1, 750, 1023, 0, 179);
+servo1.write(val1);
+delay(5); //Read finger 1
+
+val2 = analogRead(dedo2);
+val2 = map(val2, 570, 1023, 0, 179);
+servo2.write(val2);
+delay(5); //Read finger 2
+
+val3 = analogRead(dedo3);
+val3 = map(val3, 770, 1023, 0, 179);
+servo3.write(val3);
+delay(5); //Read finger 3
+
+val4 = analogRead(dedo4);
+val4 = map(val4, 1000, 1023, 0, 179);
+servo4.write(val4);
+delay(5); //Read finger 4
+
+val5 = analogRead(dedo5);
+val5 = map(val5, 720, 1023, 0, 179);
+servo5.write(val5);
+delay(5); //Read finger 5
+
+
+}
